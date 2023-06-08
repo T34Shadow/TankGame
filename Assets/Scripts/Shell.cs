adfec4b96c3c0a2650aa916cce8db350196752d1
T34Shadow <zacheryd@outlook.com>
2023-06-08 12:07:58 +1000

    SmallUpdateUplaod

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static UnityEditor.PlayerSettings;
 
 public class Shell : MonoBehaviour
 {
     public float speed;
     public GameObject Shell01;
+    public Transform ori;
     public Vector3 vel = new Vector3();
     public float gravity = 9.81f;
+    public float RayLangth = 0.7f;
 
     //before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -27,12 +31,15 @@ public class Shell : MonoBehaviour
         float desSpeed = speed * Time.deltaTime;
         transform.Translate(0f, 0f, desSpeed);
         Vector3 pos = transform.position;
-
-        bool hitSomething = Physics.Raycast(pos, Vector3.forward, out RaycastHit hitInfo, 0.5f);
+        
+        Vector3 dir = new Vector3(0, 0, 1);
+        bool hitSomething = Physics.Raycast(pos, dir, out RaycastHit hitInfo, RayLangth);
+        
+        Debug.DrawRay(pos, dir, Color.red);
         if (hitSomething)
         {
             //if (hitInfo.)
-            float overLapDepth = 0.5f - hitInfo.distance;
+            float overLapDepth = RayLangth - hitInfo.distance;
             pos.z += overLapDepth;
 
             GameObject.Destroy(Shell01);
